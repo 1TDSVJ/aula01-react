48379bf5bf401f3c1416db9011777e5935e1e6bc
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-04 10:16:02 -0300

    adicionados os componentes no App.

---
+++ rm94281/aula01/primeiro-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Cabecalho from './components/Cabecalho';
+import Carros from './components/Carros';
+import Parceiros from './components/Parceiros';
+
+export default function App() {
+  
+  return (
+    <>
+      <Cabecalho />
+      <Carros />
+      <Parceiros />
+    </>
+  );
+}
+
