99a73849610b2c4e5b0b91336ef595c8f50e1b78
LucasGuerraS <rm95084@fiap.com.br>
2022-08-04 09:52:18 -0300

    🎉 First commit: Started react project

---
+++ rm95084/aula01/primeiro-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ListaAluno from './components/ListaAlunos';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>TDS FIAP - RWD</h1>
+      <p>Alunos:</p> 
+      <p>Curso:</p>
+      <ListaAluno />
+    </div>
+  );
+}
+
+export default App;
---
+++ rm95084/aula01/primeiro-app/src/components/ListaAlunos.js
@@ -0,0 +1,13 @@
+
+
+const ListaAluno = () => {
+  return (
+    <ul>
+        <li>Aluno 1</li>
+        <li>Aluno 2</li>
+        <li>Aluno 3</li>
+    </ul>
+  );
+}
+
+export default ListaAluno;
