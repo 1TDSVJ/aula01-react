7d8db538062ef0ed1ce163c32e8ac116aa2e9131
LeonardoRCosta <rm94286@fiap.com.br>
2022-08-04 17:13:09 -0300

    Criado componente ListaAluno

---
+++ rm94286/aula01/first-app/src/components/ListaAluno.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ListaAluno() {
+
+    return(
+        <ul>
+            <li>Leonardo Ramos</li>
+            <li>Rafael Ramos</li>
+            <li>Juninho Gameplays</li>
+        </ul>
+    )
+}
